f424681c2f84d94447b7c4fe77f1c21eefdda47e
YunusNuzili <yunusnuzly619@gmail.com>
2024-05-03 00:28:15 +1000

    initialised optimisation

--- polynomial_reg.py
+++ polynomial_reg.py
@@ -6,16 +6,17 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 
 
-def optimise_price(df, item_id, store_id, target_sales, target_sales_date, current_date, year):
+def optimise_price(item_id, store_id, target_sales, target_sales_date, current_date, year):
     file_name = f"percentage_changes_decr_price/{year-1}_percentage_changes.csv"
 
     df = pd.read_csv(file_name)
 
-    base_price =  base_price(df, item_id, store_id)
-    base_demand = base_demand(df, item_id, store_id)
-    
+    base_price1 =  base_price(df, item_id, store_id)
+    base_demand1 = base_demand(df, item_id, store_id)
 
-    
+    sales_change = identify_level(df, item_id, store_id, target_sales, year, 30)
+    print(sales_change)
+        
 
 def base_price(df, item_id, store):
     dept_id = item_id[:-4]
@@ -24,13 +25,13 @@ def base_price(df, item_id, store):
         max_price =  max(temp_df['sell_price'])
     elif len(df[df['item_id'] == item_id]) > 0:
         temp_df = df[df['item_id'] == item_id]
-        max_price = max(temp_df["sell-price"])
+        max_price = max(temp_df["sell_price"])
     elif len(df[(df['dept_id'] == dept_id) & (df['store_id'] == store)]) > 0:
         temp_df = df[(df['dept_id'] == dept_id) & (df['store_id'] == store)]
-        max_price = max(temp_df["sell-price"])
+        max_price = max(temp_df["sell_price"])
     else:
         temp_df = df[df['dept_id'] == dept_id]
-        max_price = max(temp_df["sell-price"])
+        max_price = max(temp_df["sell_price"])
     return max_price
 
 def base_demand(df, item_id, store):
@@ -50,45 +51,45 @@ def base_demand(df, item_id, store):
     return max_sales
 
 
-def identify_level(df, item_id, store, sales, year, target_sales_date, price_change):
+def identify_level(df, item_id, store, sales, year, price_change):
 
     if not df[df['item_id'] == item_id].empty:
         if len(df[(df['item_id'] == item_id) & (df['store_id'] == store)]) < 20:
             if len(df[df['item_id'] == item_id]) < 20:
                 dept_id = item_id[:-4]
                 if len(df[(df['dept_id'] == dept_id) & (df['store_id'] == store)]) < 20:
-                    department_level_model(item_id, store, sales, year-1, target_sales_date, df, price_change)
+                    return department_level_model(item_id, store, sales, year-1, df, price_change)
                 else:
-                    department_store_level_model(item_id, store, sales, year-1, target_sales_date, df, price_change)
+                    return department_store_level_model(item_id, store, sales, year-1, df, price_change)
             else:
-                item_level_model(item_id, store, sales, year-1, target_sales_date, df, price_change)
+                return item_level_model(item_id, store, sales, year-1, df, price_change)
         else:
-            item_store_level_model(item_id, store, sales, year-1, target_sales_date, df, price_change)
+            return item_store_level_model(item_id, store, sales, year-1, df, price_change)
     else:
-        department_store_level_model(item_id, store, sales, year-1, target_sales_date, df, price_change)
+        return department_store_level_model(item_id, store, sales, year-1, df, price_change)
 
 
-def item_level_model(item_id, store, sales, year, target_sales_date, df, price_change):
+def item_level_model(item_id, store, sales, year, df, price_change):
     df = df[df['item_id'] == item_id]
     df['price_change'] = df['price_change'].abs()
-    fit_polynomial_model(df, price_change)
+    return fit_polynomial_model(df, price_change)
 
-def item_store_level_model(item_id, store, sales, year, target_sales_date, df, price_change):
+def item_store_level_model(item_id, store, sales, year, df, price_change):
     df = df[(df['item_id'] == item_id) & (df['store_id'] == store)]
     df['price_change'] = df['price_change'].abs()
-    fit_polynomial_model(df, price_change)
+    return fit_polynomial_model(df, price_change)
 
-def department_level_model(item_id, store, sales, year, target_sales_date, df, price_change):
+def department_level_model(item_id, store, sales, year, df, price_change):
     dept_id = item_id[:-4]
     df = df[df['dept_id'] == dept_id]
     df['price_change'] = df['price_change'].abs()
-    fit_polynomial_model(df, price_change)
+    return fit_polynomial_model(df, price_change)
 
-def department_store_level_model(item_id, store, sales, year, target_sales_date, df, price_change):
+def department_store_level_model(item_id, store, sales, year, df, price_change):
     dept_id = item_id[:-4]
     df = df[(df['dept_id'] == dept_id) & (df['store_id'] == store)]
     df['price_change'] = df['price_change'].abs()
-    fit_polynomial_model(df, price_change)
+    return fit_polynomial_model(df, price_change)
 
 
 def fit_polynomial_model(df, price_change):
@@ -115,13 +116,14 @@ def fit_polynomial_model(df, price_change):
     model.fit(X_poly, y)
 
     # Assuming X_new contains new data points for price_change
-    X_new = np.arange(0,101).reshape(-1, 1)  # Example new data
+    X_new = np.arange(101).reshape(-1, 1)  # Example new data
     # Predict sales change
 
     X_new_poly = poly_features.transform(X_new)
     # Transform new data using polynomial features
 
     y_pred = model.predict(X_new_poly)
+    print(y_pred)
 
     # Plot actual vs predicted sales change
     plt.scatter(X, y, color='blue', label='Actual Sales Change')
@@ -134,7 +136,12 @@ def fit_polynomial_model(df, price_change):
     plt.grid(True)
 
     plt.show()
-    return model.predict(price_change)
+
+    price_change_new = np.array([[price_change]])
+    print(price_change_new)
+    price_change_new_poly = poly_features.transform(price_change_new)
+    print(price_change_new_poly)
+    return model.predict(price_change_new_poly)
 
 
-identify_level('HOBBIES_1_002', 'CA_1', 500000, 2014, '12/08/2014')
+optimise_price('HOBBIES_1_002', 'CA_1', 500000, '12/08/2014', '02/06/2014', 2014)
